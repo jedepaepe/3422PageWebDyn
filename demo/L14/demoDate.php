012d602d3352604c5380484284b572fad4f7ceab
jedepaepe <jedepaepe@epfc.eu>
2015-12-17 23:24:02 +0100

    démo pour la fonctoin date() de PHP

---
+++ demo/L14/demoDate.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Démo date</title>
+    </head>
+    <body>
+        <main>
+            <h1>Démo de la fonction date()</h1>
+            <p>La fonction date permet de récupérer la date courante,
+                voir <a href='http://php.net/manual/fr/function.date.php'>
+                    le manuel PHP pour plus de détail</a></p>
+            <ul>
+                <li>date('d') : <?php echo date('d')?></li>
+                <li>date('j') : <?php echo date('j')?></li>
+            </ul>
+        </main>
+    </body>
+</html>
