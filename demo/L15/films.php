060ce41d69f66a00572582353528392e30238fb8
jedepaepe <jedepaepe@epfc.eu>
2016-01-07 22:55:41 +0100

    Elimination de code inutile. Taille différente pour la cellule "image" et la cellule "texte" (non vu en classe : un table par ligne, donc ajout d'un <div> pour le style display: table

--- demo/L15/films.php
+++ demo/L15/films.php
@@ -23,56 +23,62 @@ and open the template in the editor.
             HEADER
             <hr>
         </header>
-    <main class="twoColumns">
-        <article>
-            <div class="image">
-                <img src="Casablanca-poster.jpg" alt="Affiche du film Casablanca"/>
-            </div>
-            <div class="summary">
-                <h2>Casablanca</h2>
-                <p>
-                    Casablanca est un film américain réalisé par Michael Curtiz, sorti en 1942.
+        <main>
+            <div class="twoColumns">
+                <article>
+                    <div>
+                        <img src="http://www.udel.edu/udaily/2013/nov/images/casablanca.jpg" alt="Affiche du film Casablanca"/>
+                    </div>
+                    <div>
+                        <h2>Casablanca</h2>
+                        <p>
+                            Casablanca est un film américain réalisé par Michael Curtiz, sorti en 1942.
 
-Interprété principalement par Humphrey Bogart, Ingrid Bergman, Paul Henreid et Claude Rains, il se déroule pendant la Seconde Guerre mondiale dans la ville de Casablanca au Maroc, alors contrôlée par le gouvernement de Vichy. Le sujet majeur du film est le conflit de Rick Blaine entre l'amour et la vertu : il doit choisir entre ses sentiments pour Ilsa Lund et son besoin de faire ce qui est juste pour aider le mari de celle-ci, le héros de la Résistance, Victor Laszlo, qui doit fuir Casablanca pour continuer son combat contre les nazis.
+                            Interprété principalement par Humphrey Bogart, Ingrid Bergman, Paul Henreid et Claude Rains, il se déroule pendant la Seconde Guerre mondiale dans la ville de Casablanca au Maroc, alors contrôlée par le gouvernement de Vichy. Le sujet majeur du film est le conflit de Rick Blaine entre l'amour et la vertu : il doit choisir entre ses sentiments pour Ilsa Lund et son besoin de faire ce qui est juste pour aider le mari de celle-ci, le héros de la Résistance, Victor Laszlo, qui doit fuir Casablanca pour continuer son combat contre les nazis.
 
-Le film a connu un succès immédiat qui ne s'est pas démenti depuis. La plupart des critiques ont vanté les performances charismatiques de Bogart et Bergman, l'alchimie entre ces deux vedettes, la profondeur des personnages de fiction, la finesse du scénario ainsi que l'impact émotionnel du film dans sa globalité.
-                </p>
+                            Le film a connu un succès immédiat qui ne s'est pas démenti depuis. La plupart des critiques ont vanté les performances charismatiques de Bogart et Bergman, l'alchimie entre ces deux vedettes, la profondeur des personnages de fiction, la finesse du scénario ainsi que l'impact émotionnel du film dans sa globalité.
+                        </p>
+                    </div>
+                </article>
             </div>
-        </article>
-        <article>
-            <div class="summary">
-                <h2>Casablanca</h2>
-                <p>
-                    Casablanca est un film américain réalisé par Michael Curtiz, sorti en 1942.
+            <div class="twoColumns">
+            <article>
+                <div class="summary">
+                    <h2>Casablanca</h2>
+                    <p>
+                        Casablanca est un film américain réalisé par Michael Curtiz, sorti en 1942.
 
-Interprété principalement par Humphrey Bogart, Ingrid Bergman, Paul Henreid et Claude Rains, il se déroule pendant la Seconde Guerre mondiale dans la ville de Casablanca au Maroc, alors contrôlée par le gouvernement de Vichy. Le sujet majeur du film est le conflit de Rick Blaine entre l'amour et la vertu : il doit choisir entre ses sentiments pour Ilsa Lund et son besoin de faire ce qui est juste pour aider le mari de celle-ci, le héros de la Résistance, Victor Laszlo, qui doit fuir Casablanca pour continuer son combat contre les nazis.
+                        Interprété principalement par Humphrey Bogart, Ingrid Bergman, Paul Henreid et Claude Rains, il se déroule pendant la Seconde Guerre mondiale dans la ville de Casablanca au Maroc, alors contrôlée par le gouvernement de Vichy. Le sujet majeur du film est le conflit de Rick Blaine entre l'amour et la vertu : il doit choisir entre ses sentiments pour Ilsa Lund et son besoin de faire ce qui est juste pour aider le mari de celle-ci, le héros de la Résistance, Victor Laszlo, qui doit fuir Casablanca pour continuer son combat contre les nazis.
 
-Le film a connu un succès immédiat qui ne s'est pas démenti depuis. La plupart des critiques ont vanté les performances charismatiques de Bogart et Bergman, l'alchimie entre ces deux vedettes, la profondeur des personnages de fiction, la finesse du scénario ainsi que l'impact émotionnel du film dans sa globalité.
-                </p>
+                        Le film a connu un succès immédiat qui ne s'est pas démenti depuis. La plupart des critiques ont vanté les performances charismatiques de Bogart et Bergman, l'alchimie entre ces deux vedettes, la profondeur des personnages de fiction, la finesse du scénario ainsi que l'impact émotionnel du film dans sa globalité.
+                    </p>
+                </div>
+                <div class="image">
+                    <img src="http://www.udel.edu/udaily/2013/nov/images/casablanca.jpg" alt="Affiche du film Casablanca"/>
+                </div>
+            </article>
             </div>
-            <div class="image">
-                <img src="Casablanca-poster.jpg" alt="Affiche du film Casablanca"/>
-            </div>
-        </article>
-        <article>
-            <div class="image">
-                <img src="Casablanca-poster.jpg" alt="Affiche du film Casablanca"/>
-            </div>
-            <div class="summary">
-                <h2>Casablanca</h2>
-                <p>
-                    Casablanca est un film américain réalisé par Michael Curtiz, sorti en 1942.
+            <div class="twoColumns">
+            <article>
+                <div class="image">
+                    <img src="http://www.udel.edu/udaily/2013/nov/images/casablanca.jpg" alt="Affiche du film Casablanca"/>
+                </div>
+                <div class="summary">
+                    <h2>Casablanca</h2>
+                    <p>
+                        Casablanca est un film américain réalisé par Michael Curtiz, sorti en 1942.
 
-Interprété principalement par Humphrey Bogart, Ingrid Bergman, Paul Henreid et Claude Rains, il se déroule pendant la Seconde Guerre mondiale dans la ville de Casablanca au Maroc, alors contrôlée par le gouvernement de Vichy. Le sujet majeur du film est le conflit de Rick Blaine entre l'amour et la vertu : il doit choisir entre ses sentiments pour Ilsa Lund et son besoin de faire ce qui est juste pour aider le mari de celle-ci, le héros de la Résistance, Victor Laszlo, qui doit fuir Casablanca pour continuer son combat contre les nazis.
+                        Interprété principalement par Humphrey Bogart, Ingrid Bergman, Paul Henreid et Claude Rains, il se déroule pendant la Seconde Guerre mondiale dans la ville de Casablanca au Maroc, alors contrôlée par le gouvernement de Vichy. Le sujet majeur du film est le conflit de Rick Blaine entre l'amour et la vertu : il doit choisir entre ses sentiments pour Ilsa Lund et son besoin de faire ce qui est juste pour aider le mari de celle-ci, le héros de la Résistance, Victor Laszlo, qui doit fuir Casablanca pour continuer son combat contre les nazis.
 
-Le film a connu un succès immédiat qui ne s'est pas démenti depuis. La plupart des critiques ont vanté les performances charismatiques de Bogart et Bergman, l'alchimie entre ces deux vedettes, la profondeur des personnages de fiction, la finesse du scénario ainsi que l'impact émotionnel du film dans sa globalité.
-                </p>
+                        Le film a connu un succès immédiat qui ne s'est pas démenti depuis. La plupart des critiques ont vanté les performances charismatiques de Bogart et Bergman, l'alchimie entre ces deux vedettes, la profondeur des personnages de fiction, la finesse du scénario ainsi que l'impact émotionnel du film dans sa globalité.
+                    </p>
+                </div>
+            </article>
             </div>
-        </article>
-    </main>
-    <footer>
-        <hr>
-        FOOTER
-    </footer>
-</body>
+        </main>
+        <footer>
+            <hr>
+            FOOTER
+        </footer>
+    </body>
 </html>
